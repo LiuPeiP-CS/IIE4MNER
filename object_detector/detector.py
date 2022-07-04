475aa61bc4bd22353594f09a32cbc833067af7ed
LiuPeiP-CS <80679851+LiuPeiP-CS@users.noreply.github.com>
2022-07-04 23:51:06 +0800

    Add files via upload

---
+++ object_detector/detector.py
@@ -0,0 +1,112 @@
+import os
+import sys
+import random
+import math
+import numpy as np
+import skimage.io
+import matplotlib
+import matplotlib.pyplot as plt
+import cv2
+import torch
+
+
+# Root directory of the project
+ROOT_DIR = os.path.abspath("../")
+
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn import utils
+import mrcnn.model as modellib
+from mrcnn import visualize
+# Import COCO config
+# sys.path.append(os.path.join(ROOT_DIR, "samples/coco/"))  # To find local version
+import coco
+
+data_name = '2017'
+
+ner_img = 'XXX/data/twitter{}/ner_img'.format(data_name)
+obj_text = 'XXX/data/twitter{}/output/img_features/object_label'.format(data_name)
+save_imgs = 'XXX/data/twitter{}/vis_obj_img/'.format(data_name)
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Local path to trained weights file
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+# Download COCO trained weights from Releases if needed
+if not os.path.exists(COCO_MODEL_PATH):
+    utils.download_trained_weights(COCO_MODEL_PATH)
+
+# Directory of images to run detection on
+IMAGE_DIR = os.path.join(ROOT_DIR, "images")
+
+class InferenceConfig(coco.CocoConfig):
+    # Set batch size to 1 since we'll be running inference on
+    # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+config = InferenceConfig()
+config.display()
+
+# Create model object in inference mode.
+model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+
+# Load weights trained on MS-COCO
+model.load_weights(COCO_MODEL_PATH, by_name=True)
+
+# COCO Class names
+# Index of the class in the list is its ID. For example, to get ID of
+# the teddy bear class, use: class_names.index('teddy bear')
+class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+               'bus', 'train', 'truck', 'boat', 'traffic light',
+               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+               'kite', 'baseball bat', 'baseball glove', 'skateboard',
+               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+               'teddy bear', 'hair drier', 'toothbrush']
+
+
+# Load a random image from the images folder
+# file_names = next(os.walk(IMAGE_DIR))[2]
+# image = skimage.io.imread(os.path.join(IMAGE_DIR, random.choice(file_names)))
+images = os.listdir(ner_img)
+obj_dict = dict()
+n_count = 0
+
+for image in images:
+    # Run detection
+    try:
+        img = cv2.imread(os.path.join(ner_img, image))
+        results = model.detect([img], verbose=1)
+    except Exception as e:
+        n_count = n_count + 1
+        print(e)
+        print('错误图像：', image)
+        continue
+
+    r = results[0]
+
+    # Visualize results
+    visualize.display_instances(save_imgs+image, img, r['rois'], r['masks'], r['class_ids'], class_names, r['scores'])
+
+    img_obj_ids = []
+    # save the classes
+    for _class in r['class_ids']:
+        img_obj_ids.append(_class)
+
+    obj_dict[image] = img_obj_ids
+
+print(n_count)
+torch.save(obj_dict, obj_text)
+
+
+
